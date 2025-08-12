2f2a26382c797caae87d03eb230e8ea6998e7326
Deepak <deepakcse31@gmail.com>
2025-08-12 21:05:02 +0530

    Operation/Popup/BottomSheet

--- app/src/main/java/com/example/augustandroid/SplashActivity.java
+++ app/src/main/java/com/example/augustandroid/SplashActivity.java
@@ -25,7 +25,7 @@ public class SplashActivity extends AppCompatActivity {
                 String loginStatus=sharedPreferences.getString("loginStatus","");
                 if (loginStatus.equals("true"))
                 {
-                    Intent intent=new Intent(getApplicationContext(),WebActivity.class);
+                    Intent intent=new Intent(getApplicationContext(),BottomSheetActivity.class);
                     startActivity(intent);
                     finish();
                 }else {
